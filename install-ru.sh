02f1adf7d83c003db9f78fcbe0dcacfe8f50c6c2
MikhailTheBear <161500102+MikhailTheBear@users.noreply.github.com>
2025-03-15 11:29:09 +0300

    Create install-ru.sh

---
+++ install-ru.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+NC='\033[0m'
+
+echo "██████╗░███████╗░█████╗░██████╗░░░░░░░░█████╗░░██████╗"
+echo "██╔══██╗██╔════╝██╔══██╗██╔══██╗░░░░░░██╔══██╗██╔════╝"
+echo "██████╦╝█████╗░░███████║██████╔╝█████╗██║░░██║╚█████╗░"
+echo "██╔══██╗██╔══╝░░██╔══██║██╔══██╗╚════╝██║░░██║░╚═══██╗"
+echo "██████╦╝███████╗██║░░██║██║░░██║░░░░░░╚█████╔╝██████╔╝"
+echo "╚═════╝░╚══════╝╚═╝░░╚═╝╚═╝░░╚═╝░░░░░░░╚════╝░╚═════╝░"
+
+echo ""
+
+echo "Добро пожаловать в установщик Bear-OS!"
+echo ""
+
+echo "Хотите установить Bear-OS? (y/n)"
+read answer
+
+if [[ "$answer" == "y" || "$answer" == "Y" ]]; then
+    echo -e "${YELLOW}Загрузка Bear-OS...${NC}"
+    git clone --branch master --single-branch https://github.com/MikhailTheBear/Bear-OS.git
+    cd Bear-OS
+    echo -e "${YELLOW}Проект установлен, нажмите Enter для установки необходимых py-библиотек...${NC}"
+    read answer
+    pip install pygame colorama uuid nextcord twilio tk pyautogui
+    echo -e "${GREEN}Готово! Наслаждайтесь :)${NC}"
+    echo "Нажмите Enter, чтобы выйти..."
+    read answer
+    echo "Пока!"
+else
+    echo -e "${RED}Отменено!${NC}"
+    exit 1
+fi
