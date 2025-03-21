0b1d33aea082d5bb98ece1ce9a67e577bacb052c
MikhailTheBear <161500102+MikhailTheBear@users.noreply.github.com>
2025-03-21 23:40:27 +0300

    Update install-ru.sh

--- install-ru.sh
+++ install-ru.sh
@@ -35,6 +35,9 @@ if [[ "$answer" == "y" || "$answer" == "Y" ]]; then
     echo "phone_account_sid = 'your account_sid' " >> config.py
     echo "phone_auth_token = 'your auth_token' " >> config.py
     echo "my_phone_number = 'your phone_number' " >> config.py
+    echo "Пожалуйста, настройте SMTP [ok]"
+    read answer
+    nano config.py
     python3 MSDOS.py
     echo "Нажмите Enter, чтобы выйти..."
     read answer
