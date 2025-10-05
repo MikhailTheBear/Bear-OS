282a42da93f73185f41ab7e317968986d615cd3d
MikhailTheBear <161500102+MikhailTheBear@users.noreply.github.com>
2025-10-05 22:59:32 +0300

    Update uninstall-en.sh

--- uninstall-en.sh
+++ uninstall-en.sh
@@ -1,1 +1,35 @@
+#!/bin/bash
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+NC='\033[0m'
+
+echo "██████╗░███████╗░█████╗░██████╗░░░░░░░░█████╗░░██████╗"
+echo "██╔══██╗██╔════╝██╔══██╗██╔══██╗░░░░░░██╔══██╗██╔════╝"
+echo "██████╦╝█████╗░░███████║██████╔╝█████╗██║░░██║╚█████╗░"
+echo "██╔══██╗██╔══╝░░██╔══██║██╔══██╗╚════╝██║░░██║░╚═══██╗"
+echo "██████╦╝███████╗██║░░██║██║░░██║░░░░░░╚█████╔╝██████╔╝"
+echo "╚═════╝░╚══════╝╚═╝░░╚═╝╚═╝░░╚═╝░░░░░░░╚════╝░╚═════╝░"
+
+echo ""
+echo "Welcome to Bear-OS UNinstaller!"
+echo ""
+
+
+echo "Do you want to UNinstall Bear-OS? (y/N)"
+read -r answer
+
+if [[ "$answer" == "y" || "$answer" == "Y" ]]; then
+    echo -e "${YELLOW}Downloading Bear-OS...${NC}"
+
+    rm -rf Bear-OS
+
+    echo "Press Enter to exit..."
+    read -r
+    echo "Goodbye!"
+
+else
+    echo -e "${RED}Canceled!${NC}"
+    exit 1
+fi
